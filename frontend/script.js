d9a7451bc49a49f5b60f607ab7c7a02eb6dd5dd1
Yixin Zhao <st97334@gmail.com>
2024-01-30 02:16:03 -0500

    added search function

--- frontend/script.js
+++ frontend/script.js
@@ -1,33 +1,151 @@
 // dropdown menu of profile
 let subMenu = document.getElementById("subMenu");
 
-    function toggleMenu(){
+    function toggleMenu()
+    {
         subMenu.classList.toggle("open-menu");
     }
-
+    
 // contact list
 var myArray = [
     {
-        id : "1",
-        name : "fiona",
-        email : "potato@gmail.com",
-        phone : "324623452",
-        alternative : "linkedin or socialmedias"
+        'id' : "1",
+        'firstname' : "fiiii",
+        'lastname' : "laaaa",
+        'email' : "potato@gmail.com",
+        'phone' : "124623452",
+        'alternative' : "linkedin or socialmedias"
     },
     {
-        id : "2",
-        name : "shawn",
-        email : "frychicken@gmail.com",
-        phone : "334576552",
-        alternative : "linkedin or socialmedias"
+        'id' : "2",
+        'firstname' : "shawn",
+        'lastname' : "z",
+        'email' : "frychicken@gmail.com",
+        'phone' : "334576552",
+        'alternative' : "linkedin or socialmedias"
+    },
+    {
+        'id' : "3",
+        'firstname' : "fiiii",
+        'lastname' : "laaaa",
+        'email' : "potato@gmail.com",
+        'phone' : "999424623452",
+        'alternative' : "linkedin or socialmedias"
+    },
+    {
+        'id' : "4",
+        'firstname' : "shawn",
+        'lastname' : "z",
+        'email' : "frychicken@gmail.com",
+        'phone' : "34576552",
+        'alternative' : "linkedin or socialmedias"
+    },
+    {
+        'id' : "5",
+        'firstname' : "fiiii",
+        'lastname' : "laaaa",
+        'email' : "potato@gmail.com",
+        'phone' : "524623452",
+        'alternative' : "linkedin or socialmedias"
+    },
+    {
+        'id' : "6",
+        'firstname' : "shawn",
+        'lastname' : "z",
+        'email' : "frychicken@gmail.com",
+        'phone' : "934576552",
+        'alternative' : "linkedin or socialmedias"
     }
 ]
+
+// live search
+$('#search-input').on('keyup',function()
+{
+    var value = $(this).val()
+    console.log('Value:',value)
+    var data = searchTable(value, myArray)
+    buildTable(data)
+
+})
+
+// sort by column
+$('th').on('click', function(){
+    var column = $(this).data('colname')
+    var order = $(this).data('order')
+    var text = $(this).html()
+    text = text.substring(0, text.length - 1);
+    
+    
+    
+    if (order == 'desc'){
+        $(this).data("order","asc");
+        myArray = myArray.sort((a, b) => a[column] > b[column] ? 1 : -1)
+        text += '&#9660'
+    }else{
+        $(this).data("order","desc");
+        myArray = myArray.sort((a, b) => a[column] < b[column] ? 1 : -1)
+        text += '&#9650'
+    }
+
+   $(this).html(text)
+   buildTable(myArray)
+})
 
 buildTable(myArray)
 
 // show table data
-function showtable(curarray) {
-    document.getElementById("mytable").innerHTML = `
-        <tr class="bg
-    `;
+function buildTable(data)
+{
+    var table = document.getElementById('myTable')
+    table.innerHTML = ''
+    for (var i = 0; i < data.length; i++)
+    {
+        // var colid = `id-${i}`
+        // var colfirstname = `firstname-${i}`
+        // var collastname = `lastname-${i}`
+        // var colemail = `email-${i}`
+        // var colphone = `phone-${i}`
+        // var colalternative = `alternative-${i}`
+
+        var row = `<tr>
+                        <td>${data[i].id}</td>
+                        <td>${data[i].firstname}</td>
+                        <td>${data[i].lastname}</td>
+                        <td>${data[i].email}</td>
+                        <td>${data[i].phone}</td>
+                        <td>${data[i].alternative}</td>
+                        <td>
+                            <button><i class="fa-solid fa-pen-to-square"></i></button>
+                            <button><i class="fa-solid fa-trash-can"></i></button>
+                        </td>
+                   </tr>`
+        table.innerHTML += row
+    }
+}
+
+// search function
+function searchTable(value, data)
+{
+    var filteredData = []
+
+    for (var i = 0; i < data.length; i++)
+    {
+        value = value.toLowerCase()
+        var id = data[i].id
+        var firstname = data[i].firstname.toLowerCase()
+        var lastname = data[i].lastname.toLowerCase()
+        var email = data[i].email.toLowerCase()
+        var phone= data[i].phone
+        var alternative = data[i].alternative.toLowerCase()
+        
+        // if contains the input
+        if (id.includes(value) || firstname.includes(value)
+        || lastname.includes(value) || email.includes(value)
+        || phone.includes(value) ||alternative.includes(value))
+        {
+            // add the object
+            filteredData.push(data[i])
+        }
+    }
+    return filteredData
 }
