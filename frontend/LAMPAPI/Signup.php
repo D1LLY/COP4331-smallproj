be4c461dec5a5f506e2c9bbd78675021b66c1fc4
Soleil <20450278+soleilcordray@users.noreply.github.com>
2024-02-07 01:18:51 -0500

    Update Signup.php

--- frontend/LAMPAPI/Signup.php
+++ frontend/LAMPAPI/Signup.php
@@ -1,30 +1,41 @@
- <?php
+<?php
+    // Set the content type to application/json for the response
     header('Content-Type: application/json');
 
+    // Decode the JSON from the request
+    $inData = getRequestInfo();
+
+    // Create a connection to the database
     $conn = new mysqli("localhost", "TheBeast", "@D1llywood", "COP4331");
+
+    // Check for a connection error
     if ($conn->connect_error) {
         returnWithError($conn->connect_error);
     } else {
-        // Check if user already exists
+        // Check if the user already exists
         $stmt = $conn->prepare("SELECT ID FROM Users WHERE Login=?");
         $stmt->bind_param("s", $inData["login"]);
         $stmt->execute();
         $result = $stmt->get_result();
+        
         if ($result->num_rows > 0) {
+            // User already exists
             $stmt->close();
             $conn->close();
             returnWithError("A user with this login already exists.");
         } else {
+            // User does not exist, insert new user
             $stmt->close();
-            
-            // Insert new user
             $stmt = $conn->prepare("INSERT INTO Users (FirstName, LastName, Login, Password) VALUES(?,?,?,?)");
             $stmt->bind_param("ssss", $inData["firstName"], $inData["lastName"], $inData["login"], $inData["password"]);
+            
             if ($stmt->execute()) {
+                // Insert successful
                 $stmt->close();
                 $conn->close();
                 returnWithInfo($inData["firstName"], $inData["lastName"], $conn->insert_id);
             } else {
+                // Insert failed
                 $stmt->close();
                 $conn->close();
                 returnWithError("Failed to create user.");
@@ -32,12 +43,12 @@
         }
     }
 
+    // Functions
     function getRequestInfo() {
         return json_decode(file_get_contents('php://input'), true);
     }
 
     function sendResultInfoAsJson($obj) {
-        header('Content-type: application/json');
         echo $obj;
     }
 
